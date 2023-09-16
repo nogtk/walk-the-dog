8f9982f6ed3187320ace101c36970d2e15272f68
nogtk <taka.naoga@moneyforward.co.jp>
2023-09-17 08:41:02 +0900

    Define Renderer

--- src/engine.rs
+++ src/engine.rs
@@ -2,29 +2,49 @@ use std::{cell::RefCell, rc::Rc, sync::Mutex};
 
 use crate::browser::{self, LoopClosure};
 use anyhow::{anyhow, Result};
+use async_trait::async_trait;
 use futures::channel::oneshot::channel;
 use wasm_bindgen::{prelude::*, JsCast, JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 100.0;
+
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
-    fn draw(&self, context: &CanvasRenderingContext2d);
+    fn draw(&self, renderer: &Renderer);
 }
 
 pub struct GameLoop {
     last_frame: f64,
     accumulated_delta: f32,
 }
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
 
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
-    pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+    pub async fn start(game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
+
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
         };
+
+        let renderer = Renderer {
+            context: browser::context()?,
+        };
+
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
 
@@ -35,8 +55,9 @@ impl GameLoop {
                 game_loop.accumulated_delta -= FRAME_SIZE
             }
             game_loop.last_frame = perf;
-            game.draw(&browser::context().expect("Context should exist"));
-            browser::request_animation_frame(f.borrow().as_ref().unwrap());
+            game.draw(&renderer);
+
+            let _ = browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
 
         browser::request_animation_frame(
@@ -45,6 +66,33 @@ impl GameLoop {
                 .ok_or_else(|| anyhow!("GameLoop: Loop is None"))?,
         )?;
         Ok(())
+    }
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        );
+    }
+
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
     }
 }
 
