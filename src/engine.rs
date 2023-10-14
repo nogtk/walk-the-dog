8e869475d3c02cc2f70ee0a0bec3d3856c8b6e64
nogtk <taka.naoga@moneyforward.co.jp>
2023-10-15 01:01:47 +0900

    Remove position from Image

--- src/engine.rs
+++ src/engine.rs
@@ -195,7 +195,6 @@ impl Renderer {
 
 pub struct Image {
     element: HtmlImageElement,
-    position: Point,
     bounding_box: Rect,
 }
 
@@ -204,24 +203,22 @@ impl Image {
         let bounding_box = Rect::new(position, element.width() as i16, element.height() as i16);
         Self {
             element,
-            position,
             bounding_box,
         }
     }
     pub fn draw(&self, renderer: &Renderer) {
-        renderer.draw_entire_image(&self.element, &self.position);
+        renderer.draw_entire_image(&self.element, &self.bounding_box.position);
     }
     pub fn bounding_box(&self) -> &Rect {
         &self.bounding_box
     }
 
     pub fn move_horizontally(&mut self, distance: i16) {
-        self.set_x(self.position.x + distance);
+        self.set_x(self.bounding_box.x() + distance);
     }
 
     pub fn set_x(&mut self, x: i16) {
         self.bounding_box.set_x(x);
-        self.position.x = x;
     }
 
     pub fn right(&self) -> i16 {
