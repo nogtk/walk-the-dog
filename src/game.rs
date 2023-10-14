9c831e7faefd758521ae819097a34e651760e2e0
nogtk <taka.naoga@moneyforward.co.jp>
2023-10-15 00:16:07 +0900

    Can run infinity

--- src/game.rs
+++ src/game.rs
@@ -377,7 +377,7 @@ impl Platform {
 
 pub struct Walk {
     boy: RedHatBoy,
-    background: Image,
+    backgrounds: [Image; 2],
     stone: Image,
     platform: Platform,
 }
@@ -641,6 +641,7 @@ impl Game for WalkTheDog {
                 let json = browser::fetch_json("rhb.json").await?;
 
                 let background = engine::load_image("BG.png").await?;
+                let background_width = background.width() as i16;
                 let stone = engine::load_image("stone.png").await?;
                 let platform_sheet = browser::fetch_json("tiles.json").await?;
                 let platform = Platform::new(
@@ -657,7 +658,16 @@ impl Game for WalkTheDog {
                 );
                 Ok(Box::new(WalkTheDog::Loaded(Walk {
                     boy: rhb,
-                    background: Image::new(background, Point { x: 0, y: 0 }),
+                    backgrounds: [
+                        Image::new(background.clone(), Point { x: 0, y: 0 }),
+                        Image::new(
+                            background,
+                            Point {
+                                x: background_width,
+                                y: 0,
+                            },
+                        ),
+                    ],
                     stone: Image::new(stone, Point { x: 150, y: 547 }),
                     platform,
                 })))
@@ -684,7 +694,17 @@ impl Game for WalkTheDog {
 
             walk.platform.position.x += walk.velocity();
             walk.stone.move_horizontally(walk.velocity());
-            walk.background.move_horizontally(walk.velocity());
+            let velocity = walk.velocity();
+            let [first_background, second_background] = &mut walk.backgrounds;
+            first_background.move_horizontally(velocity);
+            second_background.move_horizontally(velocity);
+
+            if first_background.right() < 0 {
+                first_background.set_x(second_background.right());
+            }
+            if second_background.right() < 0 {
+                second_background.set_x(first_background.right());
+            }
 
             for bounding_box in &walk.platform.bounding_boxes() {
                 if walk.boy.bounding_box().intersects(bounding_box) {
@@ -715,7 +735,9 @@ impl Game for WalkTheDog {
         });
 
         if let WalkTheDog::Loaded(walk) = self {
-            walk.background.draw(renderer);
+            walk.backgrounds.iter().for_each(|background| {
+                background.draw(renderer);
+            });
             walk.boy.draw(renderer);
             walk.stone.draw(renderer);
             walk.platform.draw(renderer)
